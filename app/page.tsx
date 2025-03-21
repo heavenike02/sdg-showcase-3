f89d0b01892f0146951bc25b51bcf7ec8fcbf6ef
Heaven <heavenike02@gmail.com>
2025-03-21 19:12:16 +0000

    refactor: replace SdgFeaturesSection with CommitteeSection in home page layout for improved content organization

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/Navbar"
 import { HeroSection } from "@/components/home/HeroSection"
 import { AboutSection } from "@/components/home/AboutSection"
 import { SdgImpactSection } from "@/components/home/SdgImpactSection"
-import { SdgFeaturesSection } from "@/components/home/SdgFeaturesSection"
+
 import { CommitteeSection } from "@/components/home/CommitteeSection"
 import { NetworkSection } from "@/components/home/NetworkSection"
 
